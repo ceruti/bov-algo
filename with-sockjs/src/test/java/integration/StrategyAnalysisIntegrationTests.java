697642b7e7ce284d83451f11444c6fee47bc1de2
Marc Ceruti <marc.ceruti@imanage.com>
2020-08-28 00:13:19 -0500

    betting simulation code

--- with-sockjs/src/test/java/integration/StrategyAnalysisIntegrationTests.java
+++ with-sockjs/src/test/java/integration/StrategyAnalysisIntegrationTests.java
@@ -2,8 +2,6 @@ package integration;
 
 import lahsivjar.spring.websocket.template.Application;
 import lahsivjar.spring.websocket.template.EventBook;
-import lahsivjar.spring.websocket.template.EventRepository;
-import lahsivjar.spring.websocket.template.EventSyncService;
 import lahsivjar.spring.websocket.template.model.*;
 import lahsivjar.spring.websocket.template.util.BettingFacilitatorService;
 import lombok.AllArgsConstructor;
@@ -40,16 +38,17 @@ public class StrategyAnalysisIntegrationTests {
         mongoTemplate.getDb().getCollection("event").rename("eventDumpAug27");
     }
 
+
     @Test
     public void testBasicStrategy() {
         eventBook.setEnableUpdates(false);
         int moneyLines = 0;
-        for (Event event : eventBook.getBook().values()) {
+        List<Event> events = mongoTemplate.findAll(Event.class, "event");
+        for (Event event : events) {
             Map<String, Market> markets = event.getMarkets();
             if (markets != null) {
                 for (Market market : markets.values()) {
-                    if (market.getDescription().equalsIgnoreCase("Moneyline")
-                        || market.getDescription().equalsIgnoreCase("Fight Winner")) {
+                    if (market.getDescription().equalsIgnoreCase("Moneyline")) {
                         if (market.getOutcomes().size() == 2) {
                             List<Outcome> outcomeValues = new ArrayList<>(market.getOutcomes().values());
                             Outcome outcome1 = outcomeValues.get(0);
@@ -59,9 +58,9 @@ public class StrategyAnalysisIntegrationTests {
                                 List<Price> previousPrices2 = outcome2.getPreviousPrices();
                                 if (previousPrices1 != null
                                     && previousPrices2 != null
-                                    && previousPrices1.size() > 10
-                                    && previousPrices2.size() > 10) {
-                                    logProfit(event, market, outcome1, outcome2, previousPrices1, previousPrices2);
+                                    && previousPrices1.size() > 15
+                                    && previousPrices2.size() > 15) {
+                                    simulateBettingStrategy(event, market, outcome1, outcome2, previousPrices1, previousPrices2);
                                     moneyLines++;
                                 }
                             }
@@ -74,24 +73,98 @@ public class StrategyAnalysisIntegrationTests {
         System.out.println("Moneyline markets: "+moneyLines);
     }
 
-    private void logProfit(Event event, Market market, Outcome outcome1, Outcome outcome2, List<Price> previousPrices1, List<Price> previousPrices2) {
+    private void simulateBettingStrategy(Event event, Market market, Outcome outcome1, Outcome outcome2, List<Price> previousPrices1, List<Price> previousPrices2) {
+        List<OutcomeAndPriceTick> outcomeAndPriceTicks = getMergedPrices(outcome1, outcome2, previousPrices1, previousPrices2);
+        String winningOutcomeId = market.getBettingSession() != null && market.getBettingSession().getWinningOutcomeId() != null ?
+                market.getBettingSession().getWinningOutcomeId() : getWinningOutcome(market, outcomeAndPriceTicks);
+        if (winningOutcomeId == null) {
+            return;
+        }
         market.setBettingSession(null);
-        List<OutcomeAndPrice> outcomeAndPrices = new ArrayList<>();
+        event.setBettingEnabled(true);
+        executeBettingStrategy(event, market, outcome1, outcome2, outcomeAndPriceTicks, winningOutcomeId);
+        if (market.getBettingSession() == null) {
+            // bet was never made
+            return;
+        }
+
+//        Double finalNetProfit = market.getBettingSession().getFinalNetProfit();
+//        String finalProfit = String.format("Final profit: %.2f", finalNetProfit);
+
+        // need to know:
+        // - average profit
+        // - most profitable 20 events
+        // - least profitable 20 events
+        // - top 20 worst odds overcome... and the sport in them
+        // - top 20 odd swings and:
+        //      0.) how many odd swings
+        //      1.) end profit in top 20 sessions
+        //      2.) the sport in those sessions
+        // - average odd swings for all sports
+
+//        BettingSessionPostAnalysis postAnalysis = new BettingSessionPostAnalysis(market.getBettingSession(), market);
+//        System.out.println(finalProfit);
+    }
+
+    private List<OutcomeAndPriceTick> getMergedPrices(Outcome outcome1, Outcome outcome2, List<Price> previousPrices1, List<Price> previousPrices2) {
+        List<OutcomeAndPriceTick> outcomeAndPriceTicks = new ArrayList<>();
         for (Price price : previousPrices1) {
-            outcomeAndPrices.add(new OutcomeAndPrice(outcome1, price));
+            outcomeAndPriceTicks.add(new OutcomeAndPriceTick(outcome1, price));
         }
         for (Price price : previousPrices2) {
-            outcomeAndPrices.add(new OutcomeAndPrice(outcome2, price));
+            outcomeAndPriceTicks.add(new OutcomeAndPriceTick(outcome2, price));
         }
-        outcomeAndPrices.sort((a, b) -> a.price.getCreated().compareTo(b.price.getCreated()));
-        for (OutcomeAndPrice outcomeAndPrice : outcomeAndPrices) {
-            Outcome oppposingOutcome = outcomeAndPrice.outcome.getId().equals(outcome1.getId()) ? outcome2 : outcome1;
-            bettingFacilitatorService.updateBettingSession(event, market, outcomeAndPrice.outcome, oppposingOutcome, outcomeAndPrice.price, Strategy.BASIC);
+        outcomeAndPriceTicks.sort(Comparator.comparing(a -> a.price.getCreated()));
+        return outcomeAndPriceTicks;
+    }
+
+    private BettingExecutionMetaResults executeBettingStrategy(Event event, Market market, Outcome outcome1, Outcome outcome2, List<OutcomeAndPriceTick> outcomeAndPriceTicks, String winningOutcomeId) {
+        BettingExecutionMetaResults bettingExecutionMetaResults = new BettingExecutionMetaResults();
+        boolean initialUnderdogSet = false;
+        String lastFavoriteOutcomeId = outcomeAndPriceTicks.get(0).outcome.getId();
+        for (int i = 0; i < outcomeAndPriceTicks.size(); i++) {
+            OutcomeAndPriceTick currentTick = outcomeAndPriceTicks.get(i);
+            if (currentTick.price.getAmerican() < 100
+                    && !currentTick.outcome.getId().equals(lastFavoriteOutcomeId)) {
+                bettingExecutionMetaResults.favoriteReversals++;
+            }
+            if (currentTick.outcome.getId().equals(winningOutcomeId)) {
+                if (!initialUnderdogSet) {
+                    bettingExecutionMetaResults.winnerWasInitialUnderdog = currentTick.price.getAmerican() > 0;
+                    initialUnderdogSet = true;
+                }
+                if (currentTick.price.getAmerican() > bettingExecutionMetaResults.worstAmericanOddsForWinner) {
+                    bettingExecutionMetaResults.worstAmericanOddsForWinner = currentTick.price.getAmerican();
+                }
+            }
+            Outcome opposingOutcome = currentTick.outcome.getId().equals(outcome1.getId()) ? outcome2 : outcome1;
+            bettingFacilitatorService.updateBettingSession(event, market, currentTick.outcome, opposingOutcome, currentTick.price, Strategy.BASIC);
+            lastFavoriteOutcomeId = currentTick.outcome.getId();
         }
+        if (market.getBettingSession() != null) {
+            market.getBettingSession().setWinningOutcomeId(winningOutcomeId);
+        }
+        return bettingExecutionMetaResults;
+    }
+
+    private static class BettingExecutionMetaResults {
+        boolean winnerWasInitialUnderdog;
+        private int favoriteReversals;
+        private int worstAmericanOddsForWinner = -1000000;
+    }
+
+    private String getWinningOutcome(Market market, List<OutcomeAndPriceTick> outcomeAndPriceTicks) {
+        for (int i = outcomeAndPriceTicks.size()-1; i >= 0; i--) {
+            OutcomeAndPriceTick lastOutcomeAndPriceTick = outcomeAndPriceTicks.get(i);
+            if (lastOutcomeAndPriceTick.price.getAmerican() < 0) {
+                return lastOutcomeAndPriceTick.outcome.getId();
+            }
+        }
+        return null;
     }
 
     @AllArgsConstructor
-    private class OutcomeAndPrice {
+    private class OutcomeAndPriceTick {
         Outcome outcome;
         Price price;
     }
