a4d33525ff7d4754911b69518c413ad2486cc1b9
Marc Ceruti <marc.ceruti@imanage.com>
2020-08-27 22:41:19 -0500

    a

--- with-sockjs/src/main/java/lahsivjar/spring/websocket/template/EventSyncService.java
+++ with-sockjs/src/main/java/lahsivjar/spring/websocket/template/EventSyncService.java
@@ -1,11 +1,18 @@
 package lahsivjar.spring.websocket.template;
 
+import com.mongodb.MongoTimeoutException;
 import lahsivjar.spring.websocket.template.model.Event;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -13,13 +20,17 @@ public class EventSyncService {
 
     private EventRepository eventRepository;
     private EventBook eventBook;
+    private MongoTemplate mongoTemplate;
 
     @Autowired
-    public EventSyncService(EventRepository eventRepository, EventBook eventBook) {
+    public EventSyncService(EventRepository eventRepository, EventBook eventBook, MongoTemplate mongoTemplate) {
         this.eventRepository = eventRepository;
         this.eventBook = eventBook;
+        this.mongoTemplate = mongoTemplate;
 
-        List<Event> events = this.eventRepository.findAll();
+        Date thirtyMinutesAgo = new DateTime().minusMinutes(30).toDate();
+        List<Event> events = this.mongoTemplate.find(Query.query(Criteria.where("lastUpdated").gte(thirtyMinutesAgo)), Event.class, "event");
+
         for (Event event : events) {
             this.eventBook.getBook().put(event.getId(), event);
         }
